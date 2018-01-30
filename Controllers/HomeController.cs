e32c9926629d93c09afaecfd197ee4b9e64fb1fc
bushidian <wzq8911@msn.cn>
2018-01-31 05:15:27 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace hello.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application page.";
 
             return View();
         }
